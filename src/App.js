58fb01d76920e36b2f9898f11825ab55579bf4df
Chad Phillips <chadp0010@gmail.com>
2023-01-30 20:13:27 +0200

    Added basic Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar.js';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edi <code>src/App.js</code> and s reload.
+          Edit <code>src/App.js</code> and reload.
         </p>
         <a
           className="App-link"
@@ -15,9 +17,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          A limnk
+          React Documentation
         </a>
-        <a href="https://www.example.com"><h1>Click me!</h1></a>
       </header>
     </div>
   );
