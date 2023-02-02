4794423a708fa6ae29aeb54248239398887f5093
Chad Phillips <chadp0010@gmail.com>
2023-02-02 10:14:45 +0200

    Added content to Home and new 404 page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 function Home() {
-  return (<div>Home</div>);
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and reload...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Documentation
+        </a>
+      </header>
+    </>
+  );
 }
 export default Home
